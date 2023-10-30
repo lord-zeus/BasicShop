0869ab4a2b60e0a8ec60fe3d749fbf1b3ddc065c
lord-zeus <nsaghakingsly@gmail.com>
2023-10-30 21:30:39 +0100

    edidted the read me file

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->text,
+            'slug' => fake()->unique()->name,
+            'sku' => Str::slug(fake()->unique()->text),
+            'price' => fake()->numberBetween(100, 10000),
+            'image' => '/public/images/ZGo6ia20WYlUEAQ0GSbV8G3nO3tcADPUOcb2g907.png'
+        ];
+    }
+}
