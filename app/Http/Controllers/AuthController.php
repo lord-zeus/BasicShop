a8941b5bea333602f9395305dc8b528b763317f0
lord-zeus <nsaghakingsly@gmail.com>
2023-10-30 12:45:35 +0100

    Test-1 loging set up for admin:admin

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Traits\APIResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    use APIResponse;
+    public function login(Request $request){
+        $request->validate([
+            'name' => 'required',
+            'password' => 'required',
+        ]);
+
+        $user = User::where('name', $request->name)->first();
+
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+
+            return $this->errorResponse([
+                'name' => ['The provided credentials are incorrect.'],
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        return $this->successResponse($user->createToken($request->name)->plainTextToken);
+    }
+}
