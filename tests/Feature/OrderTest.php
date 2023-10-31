1d7e3393bb0285f3de93cedf88bb8f2203640696
lord-zeus <nsaghakingsly@gmail.com>
2023-10-31 10:53:41 +0100

    TEST-22 admin able to filter orders green

--- tests/Feature/OrderTest.php
+++ tests/Feature/OrderTest.php
@@ -9,6 +9,7 @@ use App\Models\User;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Log;
 use Tests\TestCase;
 
@@ -74,5 +75,74 @@ class OrderTest extends TestCase
             ->assertJsonIsArray("data.order_products");
         $this->assertEquals($order_products[0]['order_id'], 1);
 
+    }
+
+    public function test_admin_can_filter_orders_by_create_at_and_amount(){
+        User::factory()->create();
+        Product::factory(20)->create();
+        Order::factory()->create();
+        sleep(2);
+        Order::factory(60)->create();
+        sleep(2);
+        Order::factory()->create();
+        OrderProduct::factory(100)->create();
+        $response = $this->post('/api/v1/login', ['name'=> 'admin', 'password' => 'admin']);
+        $data = $response->getOriginalContent();
+
+
+
+        $date_response = $this->withHeaders(['Authorization' => "Bearer $data[data]"])
+            ->get('/api/v1/orders/filter/1/20?sort=amount');
+        $orders = $date_response->getContent();
+        $first_amount = 0;
+        $second_amount = 0;
+        $date_response = $this->withHeaders(['Authorization' => "Bearer $data[data]"])
+            ->get('/api/v1/orders/filter/1/20?sort=amount&order=desc');
+        $orders = $date_response->getContent();
+        $orders_j = json_decode($orders, true);
+        $third_amount = 0;
+        $forth_amount = 0;
+        foreach ($orders_j['data']['data'] as $key => $order){
+            if($key === 0)
+                $third_amount = $order['amount'];
+            else{
+                $forth_amount = $order['amount'];
+                break;
+            }
+        }
+        $date_response = $this->withHeaders(['Authorization' => "Bearer $data[data]"])
+            ->get('/api/v1/orders/filter/1/20?sort=created_at');
+        $orders = $date_response->getContent();
+        $orders_j = json_decode($orders, true);
+        $first_date = 0;
+        $second_date = 0;
+        foreach ($orders_j['data']['data'] as $key => $order){
+            if($key === 0)
+                $first_date = Carbon::make($order['created_at']);
+            else{
+                $second_date = Carbon::make($order['created_at']);
+                break;
+            }
+        }
+        $date_response = $this->withHeaders(['Authorization' => "Bearer $data[data]"])
+            ->get('/api/v1/orders/filter/1/20?sort=created_at&order=desc');
+        $orders = $date_response->getContent();
+        $orders_j = json_decode($orders, true);
+        $third = 0;
+        $forth = 0;
+        foreach ($orders_j['data']['data'] as $key => $order){
+            if($key === 0)
+                $third = Carbon::make($order['created_at']);
+            else{
+                $forth = Carbon::make($order['created_at']);
+                break;
+            }
+        }
+
+        $this->assertGreaterThanOrEqual($first_amount, $second_amount);
+        $this->assertLessThanOrEqual($third_amount, $forth_amount);
+        $this->assertTrue(Carbon::make($first_date)->lt(Carbon::make($second_date)));
+        $this->assertTrue(Carbon::make($third)->gt(Carbon::make($forth)));
+        $date_response->assertStatus(200);
     }
 }
