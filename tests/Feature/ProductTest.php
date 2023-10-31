d9b725e9267ee085341e1ed92994bf330bbd0ae7
lord-zeus <nsaghakingsly@gmail.com>
2023-10-31 06:56:33 +0100

    TEST-17 admin test view product green

--- tests/Feature/ProductTest.php
+++ tests/Feature/ProductTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Product;
 use App\Models\User;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -30,6 +31,7 @@ class ProductTest extends TestCase
     public function test_admin_can_create_product_with_valid_fields(): void
     {
         User::factory()->create();
+
         $response = $this->post('/api/v1/login', ['name'=> 'admin', 'password' => 'admin']);
         $data = $response->getOriginalContent();
         $response = $this->withHeaders(['Authorization' => "Bearer $data[data]"])
@@ -42,22 +44,36 @@ class ProductTest extends TestCase
             ]);
         $response->assertStatus(200);
         $response_data = $response->getOriginalContent();
-
         // to remove the fake created file i delete the product this feature is tested below
         $delete_product = $this->withHeaders(['Authorization' => "Bearer $data[data]"])
             ->delete("/api/v1/products/{$response_data['data']['id']}");
-
         $delete_product->assertStatus(200);
-
-
         $response = $this->withHeaders(['Authorization' => "Bearer $data[data]"])
-            ->postJson('/api/v1/products', [
+            ->post('/api/v1/products', [
                 'sku' => Str::slug(fake()->unique()->text),
                 'price' => fake()->numberBetween(100, 10000),
                 'file' => UploadedFile::fake()->image('test.jpg')
 
             ]);
+
         $response->assertStatus(422);
         $response->assertSee(['name' => "The name field is required."]);
+    }
+
+    public function test_admin_can_view_product():void
+    {
+        User::factory()->create();
+        $product = Product::factory()->create();
+        $product_object = json_decode($product);
+
+        $response = $this->post('/api/v1/login', ['name'=> 'admin', 'password' => 'admin']);
+        $data = $response->getOriginalContent();
+        $response = $this->withHeaders(['Authorization' => "Bearer $data[data]"])
+            ->get("/api/v1/products/1");
+
+        $response->assertStatus(200);
+        $response->assertSee(['name' => $product_object->name]);
+
+
     }
 }
